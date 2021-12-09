8ed514c65d5df9443739e5d615e5db34156444bc
DavidKasparyans <83000777+DavidKasparyans@users.noreply.github.com>
2021-12-09 09:30:07 +0200

    3

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,11 @@ const inputRef = document.querySelector('#validation-input');
 inputRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur() {
-  if (
-    inputRef.value.length > Number(inputRef.dataset.length) ||
-    inputRef.value.length < Number(inputRef.dataset.length)
-  ) {
-    inputRef.classList.add('invalid');
-    inputRef.classList.remove('valid');
-  } else if (inputRef.value.length === Number(inputRef.dataset.length)) {
+  if (inputRef.value.length === Number(inputRef.dataset.length)){
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
   }
 }
